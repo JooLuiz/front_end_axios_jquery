991f198ba3835e4c5108a3de5d56715874b203a9
Marbella <joo.luiz.98@outlook.com>
2019-11-04 19:45:32 -0300

    materiaUsuario crud

---
+++ Java Front/materiasUsuario/adicionarMateriaUsuario.js
@@ -0,0 +1,44 @@
+$(document).ready(function() {
+  axios.get("http://localhost:8080/myapp/materias").then(function(res) {
+    res.data.forEach(function(item) {
+      $("#materiasList").append(
+        "<option value=" + item.id + ">" + item.descricao + "</option>"
+      );
+    });
+  });
+
+  axios.get("http://localhost:8080/myapp/usuarios").then(function(res) {
+    res.data.forEach(function(item) {
+      $("#usuariosList").append(
+        "<option value=" + item.id + ">" + item.nome + "</option>"
+      );
+    });
+  });
+
+  $("#adicionarMateriaUsuario").on("submit", function(event) {
+    event.preventDefault();
+    let id = $("input[name=id]").val();
+    let ano = $("input[name=ano]").val();
+    let situacao = $("input[name=situacao]").val();
+    let materia = $("#materiasList").val();
+    let usuario = $("#usuariosList").val();
+    var materiaUsuario = {
+      ano: ano,
+      situacao: situacao,
+      materia: materia,
+      usuario: usuario
+    };
+    adicionaMateriaUsuario(materiaUsuario);
+  });
+});
+
+function adicionaMateriaUsuario(materiaUsuario) {
+  axios
+    .post("http://localhost:8080/myapp/materiasUsuario/create", materiaUsuario)
+    .then(function(res) {
+      console.log(res);
+    })
+    .finally(function() {
+      window.history.back();
+    });
+}
---
+++ Java Front/materiasUsuario/materiasUsuario.js
@@ -0,0 +1,114 @@
+$(document).ready(function() {
+  axios.get("http://localhost:8080/myapp/materiasUsuario").then(function(res) {
+    res.data.forEach(function(item) {
+      $("#listaMateriasUsuarioBody").append(
+        "<tr><td>" +
+          item.ano +
+          "</td>" +
+          "<td>" +
+          item.situacao +
+          "</td>" +
+          "<td>" +
+          item.materia.descricao +
+          "</td>" +
+          "<td>" +
+          item.usuario.nome +
+          "</td>" +
+          "<td><a href='javascript:editMateriaUsuario(" +
+          item.id +
+          ")'>Editar</td>" +
+          "<td><a href='javascript:deleteMateriaUsuario(" +
+          item.id +
+          ")'>Excluir</td></tr>"
+      );
+    });
+  });
+
+  axios.get("http://localhost:8080/myapp/materias").then(function(res) {
+    res.data.forEach(function(item) {
+      $("#materiasList").append(
+        "<option value=" + item.id + ">" + item.descricao + "</option>"
+      );
+    });
+  });
+
+  axios.get("http://localhost:8080/myapp/usuarios").then(function(res) {
+    res.data.forEach(function(item) {
+      $("#usuariosList").append(
+        "<option value=" + item.id + ">" + item.nome + "</option>"
+      );
+    });
+  });
+
+  var modal = document.getElementById("editMateriaUsuarioModal");
+
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close")[0];
+
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
+    modal.style.display = "none";
+  };
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  };
+
+  $("#doEditMateriaUsuario").on("click", function() {
+    let id = $("input[name=id]").val();
+    let ano = $("input[name=ano]").val();
+    let situacao = $("input[name=situacao]").val();
+    let materia = $("#materiasList").val();
+    let usuario = $("#usuariosList").val();
+    var materiaUsuario = {
+      ano: ano,
+      situacao: situacao,
+      materia: materia,
+      usuario: usuario
+    };
+    doEditMateriaUsuario(materiaUsuario, id);
+  });
+});
+
+function doEditMateriaUsuario(materiaUsuario, id) {
+  axios
+    .put(
+      "http://localhost:8080/myapp/materiasUsuario/materiaUsuario/" + id,
+      materiaUsuario
+    )
+    .then(function(res) {
+      console.log(res);
+    })
+    .finally(function() {
+      location.reload();
+    });
+}
+
+function editMateriaUsuario(id) {
+  // Get the modal
+  axios
+    .get("http://localhost:8080/myapp/materiasUsuario/materiaUsuario/" + id)
+    .then(function(res) {
+      $("input[name=id]").val(res.data.id);
+      $("input[name=ano]").val(res.data.ano);
+      $("input[name=situacao]").val(res.data.situacao);
+      $("#materiasList").val(res.data.materia.id);
+      $("#usuariosList").val(res.data.usuario.id);
+      var modal = document.getElementById("editMateriaUsuarioModal");
+      modal.style.display = "block";
+    });
+}
+
+function deleteMateriaUsuario(id) {
+  axios
+    .delete("http://localhost:8080/myapp/materiasUsuario/materiaUsuario/" + id)
+    .then(function(res) {
+      console.log(res);
+    })
+    .finally(function() {
+      location.reload();
+    });
+}
